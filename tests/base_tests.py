abdcc06e15902559736ae8f176e7f77583c74f46
Andy Sayler <andy.sayler@gmail.com>
2014-10-24 16:48:04 -0600

    Added base tests

---
+++ tests/base_tests.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+
+# Andy Sayler
+# Summer 2014
+# Univerity of Colorado
+
+
+import unittest
+
+import keyval.base
+
+
+_TEST_KEY_PRE = "TESTKEY"
+_TEST_VAL_PRE_STRING = "TESTVAL"
+
+
+class BaseTestCase(unittest.TestCase):
+
+    def __init__(self, *args, **kwargs):
+        super(BaseTestCase, self).__init__(*args, **kwargs)
+
+    def setUp(self):
+        super(BaseTestCase, self).setUp()
+        self.key_cnt = 0
+        self.val_cnt = 0
+
+    def tearDown(self):
+        super(BaseTestCase, self).tearDown()
+
+    def generate_key(self):
+        key = "{:s}_{:d}".format(_TEST_KEY_PRE, self.key_cnt)
+        self.key_cnt += 1
+        return key
+
+class PersistentObjectMixin(object):
+
+    def __init__(self, *args, **kwargs):
+        super(PersistentObjectMixin, self).__init__(*args, **kwargs)
+
+    def test_from_new_good(self):
+
+        # Setup Test Vals
+        key = self.generate_key()
+        val = self.generate_val()
+
+        # Create New Instance
+        instance = self.factory.from_new(key, val)
+        self.assertTrue(instance)
+        self.assertTrue(instance.exists())
+        self.assertEqual(key, instance.key())
+        self.assertEqual(val, instance.val())
+
+        # Cleanup
+        instance.rem()
+
+class StringMixin(PersistentObjectMixin):
+
+    def __init__(self, *args, **kwargs):
+        super(StringMixin, self).__init__(*args, **kwargs)
+        self.factory = keyval.base.InstanceFactory(self.driver, self.module.String)
+
+    def generate_val(self):
+        val = "{:s}_{:d}".format(_TEST_VAL_PRE_STRING, self.val_cnt)
+        self.val_cnt += 1
+        return val
