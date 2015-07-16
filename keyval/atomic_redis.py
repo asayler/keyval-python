24cad77cbeb9b2cf3c461b44c56c9360d464ac87
Andy Sayler <andy.sayler@gmail.com>
2015-07-15 22:07:56 -0400

    Re-added atomic string functions

--- keyval/atomic_redis.py
+++ keyval/atomic_redis.py
@@ -9,6 +9,7 @@
 
 import redis
 
+import base
 import base_redis
 import atomic_abc
 
@@ -36,7 +37,220 @@ class String(Sequence,
 
 class MutableString(MutableSequence, String,
                     base_redis.MutableString, atomic_abc.String):
-    pass
+
+    def __setitem__(self, idx, itm):
+        """Set Seq Item"""
+
+        # Check Input
+        itm = str(itm)
+        if len(itm) != 1:
+            raise ValueError("{:s} must be a single charecter".format(v))
+
+        # Transaction
+        def automic_setitem(pipe):
+
+            # Check Exists
+            if not self._exists(pipe):
+                raise base.ObjectDNE(self)
+
+            # Check Index
+            length = pipe.strlen(self._redis_key)
+            if (idx >= length) or (idx < -length):
+                raise IndexError("{:d} out of range".format(idx))
+
+            # Normalize Index
+            if (idx >= 0):
+                idx_norm = idx
+            else:
+                idx_norm = length + idx
+
+            # Set Item
+            pipe.multi()
+            pipe.setrange(self._redis_key, idx_norm, itm)
+
+        # Execute Transaction
+        self._driver.transaction(automic_setitem, self._redis_key)
+
+    def insert(self, idx, itm):
+        """Insert Seq Item"""
+
+        # Validate Input
+        itm = str(itm)
+        if len(itm) != 1:
+            raise ValueError("{:s} must be a single charecter".format(itm))
+
+        # Transaction
+        def automic_insert(pipe):
+
+            # Check Exists
+            if not self._exists(pipe):
+                raise base.ObjectDNE(self)
+
+            # Get Ranges
+            length = pipe.strlen(self._redis_key)
+            if (idx == 0) or (idx <= -length):
+                start = ""
+            else:
+                start = pipe.getrange(self._redis_key, 0, (idx-1))
+            if (idx >= length):
+                end = ""
+            else:
+                end = pipe.getrange(self._redis_key, idx, length)
+
+            # Set New Val
+            new = start + itm + end
+            pipe.multi()
+            pipe.set(self._redis_key, new)
+
+        # Execute Transaction
+        self._driver.transaction(automic_insert, self._redis_key)
+
+    def append(self, itm):
+        """Append Seq Item"""
+
+        # Validate Input
+        v = str(itm)
+        if len(itm) != 1:
+            raise ValueError("{:s} must be a single charecter".format(itm))
+
+        # Transaction
+        def automic_append(pipe):
+
+            # Check Exists
+            if not self._exists(pipe):
+                raise base.ObjectDNE(self)
+
+            # Append
+            pipe.multi()
+            pipe.append(self._redis_key, itm)
+
+        # Execute Transaction
+        self._driver.transaction(automic_append, self._redis_key)
+
+    def extend(self, seq):
+        """Append Seq wuth another Seq"""
+
+        # Validate Input
+        seq = str(seq)
+
+        # Transaction
+        def automic_extend(pipe):
+
+            # Check Exists
+            if not self._exists(pipe):
+                raise base.ObjectDNE(self)
+
+            # Extend
+            pipe.multi()
+            pipe.append(self._redis_key, seq)
+
+        # Execute Transaction
+        if len(seq):
+            self._driver.transaction(automic_extend, self._redis_key)
+        else:
+            pass
+
+    def reverse(self):
+        """Reverse Seq"""
+
+        # Transaction
+        def automic_reverse(pipe):
+
+            # Check Exists
+            if not self._exists(pipe):
+                raise base.ObjectDNE(self)
+
+            # Read
+            seq = pipe.get(self._redis_key)
+
+            # Reverse
+            rev = seq[::-1]
+
+            # Write
+            pipe.multi()
+            pipe.set(self._redis_key, rev)
+
+        # Execute Transaction
+        self._driver.transaction(automic_reverse, self._redis_key)
+
+    def pop(self, pop_idx=None):
+        """Pop Seq Item"""
+
+        # Transaction
+        def automic_pop(pipe):
+
+            # Check Exists
+            if not self._exists(pipe):
+                raise base.ObjectDNE(self)
+
+            # Check Index
+            length = pipe.strlen(self._redis_key)
+            if pop_idx is None:
+                idx = (length-1)
+            else:
+                idx = pop_idx
+            if (idx >= length) or (idx < -length):
+                raise IndexError("{:d} out of range".format(idx))
+
+            # Get Ranges
+            if (idx == 0) or (idx == -length):
+                start = ""
+            else:
+                start = pipe.getrange(self._redis_key, 0, (idx-1))
+            if (idx == (length-1)) or (idx == -1):
+                end = ""
+            else:
+                end = pipe.getrange(self._redis_key, (idx+1), length)
+
+            # Set New Val
+            new = start + end
+            pipe.multi()
+            pipe.getrange(self._redis_key, idx, idx)
+            pipe.set(self._redis_key, new)
+
+        # Execute Transaction
+        ret = self._driver.transaction(automic_pop, self._redis_key)
+        return str(ret[0])
+
+    def remove(self, itm):
+        """Remove itm from Seq"""
+
+        # Validate Input
+        itm = str(itm)
+        if len(itm) != 1:
+            raise ValueError("{:s} must be a single charecter".format(itm))
+
+        # Transaction
+        def automic_remove(pipe):
+
+            # Check Exists
+            if not self._exists(pipe):
+                raise base.ObjectDNE(self)
+
+            # Get idx
+            seq = pipe.get(self._redis_key)
+            idx = seq.index(itm)
+
+            # Get Ranges
+            length = pipe.strlen(self._redis_key)
+            if (idx == 0):
+                start = ""
+            else:
+                start = pipe.getrange(self._redis_key, 0, (idx-1))
+            if (idx == (length-1)):
+                end = ""
+            else:
+                end = pipe.getrange(self._redis_key, (idx+1), length)
+
+            # Set New Val
+            new = start + end
+
+            # Write
+            pipe.multi()
+            pipe.set(self._redis_key, new)
+
+        # Execute Transaction
+        self._driver.transaction(automic_remove, self._redis_key)
 
 class List(Sequence,
            base_redis.List, atomic_abc.List):
