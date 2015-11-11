ebd40067c97389a16ef03d582799fe9f1ceeaba9
Andy Sayler <andy.sayler@gmail.com>
2015-11-11 16:45:38 -0700

    Added python3 test stubs

---
+++ tests/run_py3_atomic_redis_tests.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+# Andy Sayler
+# 2014, 2015
+# pcollections Tests
+
+
+from atomic_redis_tests import *
+if __name__ == '__main__':
+    unittest.main()
